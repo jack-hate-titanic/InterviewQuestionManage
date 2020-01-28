ef3cbcbf47963739cdc14a315241ea593706a796
wangsen <1002783067@qq.com>
2020-01-28 11:14:01 +0800

    fix:delete priority

--- typings/app/model/index.d.ts
+++ typings/app/model/index.d.ts
@@ -3,13 +3,11 @@
 
 import 'egg';
 import ExportJs = require('../../../app/model/js');
-import ExportPriority = require('../../../app/model/priority');
 import ExportUser = require('../../../app/model/user');
 
 declare module 'egg' {
   interface IModel {
     Js: ReturnType<typeof ExportJs>;
-    Priority: ReturnType<typeof ExportPriority>;
     User: ReturnType<typeof ExportUser>;
   }
 }
