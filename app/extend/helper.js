eddcf269f85b5b9067ebda8a7bab2d84eebd85bb
Y-wson <1002783067@qq.com>
2021-06-20 21:55:54 +0800

    fix:修改过期时间

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,11 +1,10 @@
-const jwt = require('jsonwebtoken') //引入jsonwebtoken
-
+const jwt = require("jsonwebtoken"); //引入jsonwebtoken
 
 //token是密匙
 module.exports = {
-    loginToken(data, expires = 7200) {
-        const exp = Math.floor(Date.now() / 1000) + expires;
-        const token = jwt.sign({data, exp}, 'token');
-        return token
-    }
-}
+  loginToken(data, expires = 86400) {
+    const exp = Math.floor(Date.now() / 1000) + expires;
+    const token = jwt.sign({ data, exp }, "token");
+    return token;
+  },
+};
